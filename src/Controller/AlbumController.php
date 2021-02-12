0ec9c608ae817252813b5df4c24ff8e10b2eb977
Macsym2018 <43283659+Macsym2018@users.noreply.github.com>
2021-02-12 09:43:33 +0200

    add form with styles to module

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -23,4 +23,21 @@ class AlbumController extends ControllerBase {
     ];
   }
 
+  /**
+   * Return Content.
+   *
+   * @return array
+   *   Return text
+   */
+  public function formContent() {
+
+    $form = \Drupal::formBuilder()->getForm('Drupal\photos\Form\PhotosForm');
+    $form['#attached']['library'][] = 'photos/global-styling';
+
+    return [
+      '#theme' => 'photos_template',
+      '#form' => $form,
+    ];
+  }
+
 }
